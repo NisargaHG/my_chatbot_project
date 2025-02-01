1d16a6e90ce360dc3d6140d16117b42a5d24a513
NisargaHG <146320762+NisargaHG@users.noreply.github.com>
2025-02-01 11:04:31 +0530

    Update settings.py

--- chatbot_project/chatbot_project/settings.py
+++ chatbot_project/chatbot_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8*5k@ie6t)%er-#34vj5$xggil6fqcr__qwdkl0_4swqt7t!16'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 ALLOWED_HOSTS = ['nisarga chatbotproject.netlify.app', 'localhost', '127.0.0.1']
